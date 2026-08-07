219e8f25bebe29ac764ede99fae590290a97b22b
aigul <ysakova90@gmail.com>
2026-08-07 00:28:16 +0000

    updated workflow

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "kubernetes_namespace" "example" {
   metadata {
     annotations = var.annotations
-    labels = var.labels
-    name = var.name
+    labels      = var.labels
+    name        = var.name
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,23 +1,21 @@
 variable "name" {
-    description = "namespace name in k8s"
-    type = string
-    default = "demo"
+  description = "namespace name in k8s"
+  type        = string
+  default     = "demo"
 }
 
 variable "labels" {
-    description = "labels to used in namespace"
-    type = map(string)
-    default = {
-      mylabel  = "label-value"
-    }
-  
+  description = "labels to used in namespace"
+  type        = map(string)
+  default = {
+    mylabel = "label-value"
+  }
 }
 
 variable "annotations" {
-    description = "annotations to used in namespace"
-    type = map(string)
-    default = {
-      mylabel  = "label-value"
-    }
-  
+  description = "annotations to used in namespace"
+  type        = map(string)
+  default = {
+    mylabel = "label-value"
+  }
 }
